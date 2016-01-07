f594cdecb06da07f2993567ff37ba73df3fdaaf5
Dimitri Dewaele <dimitri.dewaele@mips.be>
2016-01-07 13:59:02 +0100

    test with locale

--- src/main/java/be/softwarelab/testprogram/TestProgram.java
+++ src/main/java/be/softwarelab/testprogram/TestProgram.java
@@ -1,5 +1,7 @@
 package be.softwarelab.testprogram;
 
+import java.util.Locale;
+
 public class TestProgram {
     
     private static final org.slf4j.Logger LOGGER = org.slf4j.LoggerFactory.getLogger(TestProgram.class);
@@ -11,6 +13,20 @@ public class TestProgram {
     public static void main(String[] args) {
         LOGGER.info("Test Program: START");
         CONSOLE.info("Test Program: START");
+        
+        String sentence = null;
+        String start = "This is";
+        String day = "Day";
+        
+        sentence = start + " first " + day.toLowerCase();
+        CONSOLE.info(sentence);
+        
+        sentence = start + " first " + day.toLowerCase(Locale.ENGLISH);
+        CONSOLE.info(sentence);
+        
+        sentence = start + " first " + day.toLowerCase(Locale.GERMAN);
+        CONSOLE.info(sentence);
+        
         // TODO code application logic here
         LOGGER.info("Test Program: END");
         CONSOLE.info("Test Program: END");
